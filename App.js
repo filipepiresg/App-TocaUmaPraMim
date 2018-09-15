08bd329d3387ba099443684e31cca5d2e18ef162
Filipe Pires <liipepires@gmail.com>
2018-09-15 00:48:47 -0300

    add reactotron ao projeto

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import * as firebase from 'firebase'
+import React from "react";
+import * as firebase from "firebase";
 
-import { MainNavigator } from './src/navigators'
+import { MainNavigator } from "./src/navigators";
+import "./ReactotronConfig";
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     const firebaseConfig = {
-      apiKey: 'AIzaSyBUg1w5wWR2DttpKi5WKC3MTftqqlVbKZs',
-      authDomain: 'tupm-app.firebaseapp.com',
-      databaseURL: 'https://tupm-app.firebaseio.com',
-      projectId: 'tupm-app',
-      storageBucket: 'tupm-app.appspot.com',
-      messagingSenderId: '558789255926',
-    }
-    firebase.initializeApp(firebaseConfig)
+      apiKey: "AIzaSyBUg1w5wWR2DttpKi5WKC3MTftqqlVbKZs",
+      authDomain: "tupm-app.firebaseapp.com",
+      databaseURL: "https://tupm-app.firebaseio.com",
+      projectId: "tupm-app",
+      storageBucket: "tupm-app.appspot.com",
+      messagingSenderId: "558789255926"
+    };
+    firebase.initializeApp(firebaseConfig);
   }
+
   render() {
-    return <MainNavigator />
+    return <MainNavigator />;
   }
 }
