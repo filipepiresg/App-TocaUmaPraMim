3878f79f6528b5707026722f4d00f7cf82dfdfb1
Luciano Júnior <luciannojunior@gmail.com>
2018-09-20 19:05:08 -0300

    Removing unused imports

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Dimensions, AsyncStorage } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import {
   Container,
   Button,
