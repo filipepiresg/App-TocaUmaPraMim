f4b8f3a963c1aee6b639030a721950147b6b7eb7
Luciano Júnior <luciannojunior@gmail.com>
2018-09-19 23:54:02 -0300

    Ok

--- src/components/hocs/withAuth.js
+++ src/components/hocs/withAuth.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as firebase from 'firebase';
-
+require("firebase/firestore");
 const _loginWithFacebook = async () => {
   const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
     '2207895276121269',
@@ -13,7 +13,7 @@ const _loginWithFacebook = async () => {
 
     // Sign in with credential from the Facebook user.
     firebase.auth().signInAndRetrieveDataWithCredential(credential).then(({ user })=>{
-      console.log(user);
+      _verifyUser(user);
     }).catch((error) => {
       console.log('Esse erro deve ser tratado e deve ser exibido um feedback para o usuário');
       console.log(error);
@@ -23,7 +23,23 @@ const _loginWithFacebook = async () => {
   }
 }
 
-const _verifyUser = async (uid) => {  }
+/**
+ * Verifies if this uid is registered in the app. If not, redirect the user to the RegisterScreen.
+ * If it is, redirects him/her to the Main Screen.
+ * 
+ * @param {String} uid 
+ */
+const _verifyUser = async (user) => { 
+  const db = firebase.firestore();
+  db.settings({timestampsInSnapshots: true})
+  const response = await db.collection('users').where('authId', '==', user.uid).get()
+  if (response.empty){
+    // Redirect to Register Screen
+    // Pass params 
+  } else {
+    // Redirect to Main App
+  }
+}
 
 export default (WrappedComponent) => {
   class withAuth extends React.Component {
