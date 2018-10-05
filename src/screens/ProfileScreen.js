ee8fb13ea265bfa932d3e0fcf6bba061996ca968
Caio Melo <caiofelipee17@gmail.com>
2018-10-04 21:27:08 -0300

    Fix profileScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -141,85 +141,60 @@ export default class ProfileScreen extends Component {
     const { navigation } = this.props;
     return (
       <Container style={styles.container}>
-<<<<<<< HEAD
-        <Header transparent>
-          {
-            navigation.getParam("back", false) ? (
-              <Left>
-                <Button onPress={() => navigation.goBack()} transparent>
-                  <Icon type="FontAwesome" name="angle-left" />
-                </Button>
-              </Left>
-            ) : (
-              <Left />
-            )
-          }
-          <Body>
-            <Title>Profile</Title>
-          </Body>
-          <Right>
-          <Button onPress={() => navigation.goBack()} transparent>
-           <Text   color="#841584">edit </Text>
-          </Button>
-          _  
-          </Right>
-        </Header>
-=======
->>>>>>> 37a9871e0d9dd084fa6a901efc56153bfb9a1c5e
-        <Container style={styles.subContainer}>
-          <Container style={styles.containerPerfil}>
-            <Image
-              source={photoURL ? { uri: photoURL } : imgDefault}
-              style={styles.imgPerfil}
-              resizeMode="cover"
-            />
-            <Title>{name}</Title>
-            <Subtitle>{instrument}</Subtitle>
-            <View style={styles.containerInfo}>
-              <View style={styles.typeInfo}>
-                <H1>{inventory ? inventory.length : 0}</H1>
-                <Text style={styles.txtInfo}>músicas diferentes</Text>
-              </View>
-              <View style={styles.typeInfo}>
-                <H1>{ amountArtists(inventory) }</H1>
-                <Text style={styles.txtInfo}>artistas diferentes</Text>
-              </View>
-              <View style={styles.typeInfo}>
-                <H1>{ amountRitmos(inventory) }</H1>
-                <Text style={styles.txtInfo}>ritmos diferentes</Text>
-              </View>
+      <Container style={styles.subContainer}>
+        <Container style={styles.containerPerfil}>
+          <Image
+            source={photoURL ? { uri: photoURL } : imgDefault}
+            style={styles.imgPerfil}
+            resizeMode="cover"
+          />
+          <Title>{name}</Title>
+          <Subtitle>{instrument}</Subtitle>
+          <View style={styles.containerInfo}>
+            <View style={styles.typeInfo}>
+              <H1>{inventory ? inventory.length : 0}</H1>
+              <Text style={styles.txtInfo}>músicas diferentes</Text>
             </View>
-          </Container>
-          <Item>
-              <Icon type="FontAwesome"
-                    name="plus"
-                    style={{ color: "#11841a" }}
-                    onPress={() => navigation.navigate("MusicRegistration")}
-              />
-              <Item style={styles.itemSearch}>
-                  <DebouncedInputComponent
-                      placeholder="busca por música, ritmo e/ou banda"
-                      updateText={this.searchSong}
-                      style={styles.inputSearch}
-                  />
-              </Item>
-          </Item>
-          <Content contentContainerStyle={styles.content}>
-            {/*inventory
-              .filter(
-                value =>
-                  value.music.toLowerCase().includes(search.toLowerCase()) ||
-                  value.group.toLowerCase().includes(search.toLowerCase()) ||
-                  value.ritmo.toLowerCase().includes(search.toLowerCase())
-              )
-              .map(item => (
-                <Text key={item.music} style={{ padding: 5 }}>{`${
-                  item.music
-                } - ${item.group} - ${item.ritmo}`}</Text>
-              )) */}
-          </Content>
+            <View style={styles.typeInfo}>
+              <H1>{ amountArtists(inventory) }</H1>
+              <Text style={styles.txtInfo}>artistas diferentes</Text>
+            </View>
+            <View style={styles.typeInfo}>
+              <H1>{ amountRitmos(inventory) }</H1>
+              <Text style={styles.txtInfo}>ritmos diferentes</Text>
+            </View>
+          </View>
         </Container>
+        <Item>
+            <Icon type="FontAwesome"
+                  name="plus"
+                  style={{ color: "#11841a" }}
+                  onPress={() => navigation.navigate("MusicRegistration")}
+            />
+            <Item style={styles.itemSearch}>
+                <DebouncedInputComponent
+                    placeholder="busca por música, ritmo e/ou banda"
+                    updateText={this.searchSong}
+                    style={styles.inputSearch}
+                />
+            </Item>
+        </Item>
+        <Content contentContainerStyle={styles.content}>
+          {/*inventory
+            .filter(
+              value =>
+                value.music.toLowerCase().includes(search.toLowerCase()) ||
+                value.group.toLowerCase().includes(search.toLowerCase()) ||
+                value.ritmo.toLowerCase().includes(search.toLowerCase())
+            )
+            .map(item => (
+              <Text key={item.music} style={{ padding: 5 }}>{`${
+                item.music
+              } - ${item.group} - ${item.ritmo}`}</Text>
+            )) */}
+        </Content>
       </Container>
+    </Container>
     );
   }
 }
