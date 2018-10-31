d8d6555b81c59984288aa3ecc009434f18840433
Filipe Pires <liipepires@gmail.com>
2018-10-31 01:15:09 -0300

    refactoring from navigators

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -16,9 +16,83 @@ import stylesd from '../stylesd';
 const ACTIVE_COLOR = '#fff'
 const INACTIVE_COLOR = stylesd.corDeFundo
 
-const TabNavigator = createBottomTabNavigator({ 
-  Explore:{
-    screen: ExploreScreen,
+// const TabNavigator = createBottomTabNavigator({ 
+//   Explore:{
+//     screen: ExploreScreen,
+//     navigationOptions:{
+//       tarBarLabel: translate("explore"),
+//       tabBarIcon:({ tintColor }) => (
+//         <Icon name="globe" style={{color:tintColor}} size={24}/>
+//       )
+//     }
+//   },
+//   Profile:{
+//     screen: ProfileScreen,
+//     navigationOptions:{
+//       tabBarLabel: translate("profile"),
+//       tabBarIcon: ({ tintColor }) => (
+//         <Icon name="ios-person" style={{color:tintColor}} size={24}/>
+//       )
+//     }
+//   },
+//   Configurations:{
+//     screen: ConfigurationScreen,
+//     navigationOptions:{
+//       tabBarLabel: translate("configurations"),
+//       tabBarIcon: ({ tintColor }) => (
+//         <Icon active name="settings" style={{color:tintColor}} size={24}/>
+//       )
+//     }
+//   }
+// },{//router cconfig
+//   initialRouteName: 'Profile',
+//   order: ['Explore','Profile','Configurations'],
+//   navigationOptions:{
+//     tabBarVisible: true
+//   },
+//   tabBarOptions:{
+//     activeTintColor: ACTIVE_COLOR,
+//     inactiveTintColor: INACTIVE_COLOR,
+//     style: {
+//       backgroundColor: stylesd.segundaCor
+//     }
+//   }
+  
+// });
+
+const ExploreStack = createStackNavigator({
+  Explore: ExploreScreen,
+  Artist: ArtistScreen,
+  }, {
+    initialRouteKey: 'Exlore',
+    navigationOptions: {
+      headerTransparent: true
+    }
+});
+
+const ProfileStack = createStackNavigator({
+  Profile: ProfileScreen,
+  EditProfile: EditProfileScreen,
+  NewSong: NewSongScreen,
+  }, {
+    initialRouteKey:'Profile',
+    navigationOptions: {
+      headerTransparent: true
+    }
+});
+
+const SettingsStack = createStackNavigator({
+  Configurations: ConfigurationScreen
+  }, {
+    initialRouteKey:'Profile',
+    navigationOptions: {
+      headerTransparent: true
+    }
+});
+
+export default createBottomTabNavigator({
+  Explore: {
+    screen: ExploreStack,
     navigationOptions:{
       tarBarLabel: translate("explore"),
       tabBarIcon:({ tintColor }) => (
@@ -27,7 +101,7 @@ const TabNavigator = createBottomTabNavigator({
     }
   },
   Profile:{
-    screen: ProfileScreen,
+    screen: ProfileStack,
     navigationOptions:{
       tabBarLabel: translate("profile"),
       tabBarIcon: ({ tintColor }) => (
@@ -36,7 +110,7 @@ const TabNavigator = createBottomTabNavigator({
     }
   },
   Configurations:{
-    screen: ConfigurationScreen,
+    screen: SettingsStack,
     navigationOptions:{
       tabBarLabel: translate("configurations"),
       tabBarIcon: ({ tintColor }) => (
@@ -44,37 +118,36 @@ const TabNavigator = createBottomTabNavigator({
       )
     }
   }
-},{//router cconfig
-  initialRouteName: 'Profile',
-  order: ['Explore','Profile','Configurations'],
-  navigationOptions:{
-    tabBarVisible: true
-  },
-  tabBarOptions:{
-    activeTintColor: ACTIVE_COLOR,
-    inactiveTintColor: INACTIVE_COLOR,
-    style: {
-      backgroundColor: stylesd.segundaCor
+}, {//router cconfig
+    initialRouteName: 'Profile',
+    order: ['Explore','Profile','Configurations'],
+    navigationOptions:{
+      tabBarVisible: true
+    },
+    tabBarOptions:{
+      activeTintColor: ACTIVE_COLOR,
+      inactiveTintColor: INACTIVE_COLOR,
+      style: {
+        backgroundColor: stylesd.segundaCor
+      }
     }
-  }
-  
 });
 
-export default createStackNavigator({
-    TabNavigator:{
-      screen: TabNavigator,
-      navigationOptions: {
-        header: null
-      }
-    },
-    EditProfile: EditProfileScreen,
-    NewSong: NewSongScreen,
-    Artist: ArtistScreen,
-  }, {
-    headerMode: 'float',
-    initialRouteKey: 'TabNavigator',
-    navigationOptions: {
-      headerTransparent: true,
-    }
-  }
-)
+// export default createStackNavigator({
+//     TabNavigator:{
+//       screen: TabNavigator,
+//       navigationOptions: {
+//         header: null
+//       }
+//     },
+//     EditProfile: EditProfileScreen,
+//     NewSong: NewSongScreen,
+//     Artist: ArtistScreen,
+//   }, {
+//     headerMode: 'float',
+//     initialRouteKey: 'TabNavigator',
+//     navigationOptions: {
+//       headerTransparent: true,
+//     }
+//   }
+// )
