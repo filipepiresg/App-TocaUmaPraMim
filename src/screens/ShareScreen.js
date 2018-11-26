083f170eba0e0a8944ec6709889ef9b242b62f74
Caio Melo <caiofelipee17@gmail.com>
2018-11-25 22:21:19 -0300

    Add share button

---
+++ src/screens/ShareScreen.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, AsyncStorage, Dimensions } from 'react-native'
+import { Container, Button, Content, View } from 'native-base'
+import * as firebase from 'firebase'
+import { styles as s } from 'react-native-style-tachyons'
+import stylesd from '../stylesd'
+import translate from '../i18n/src/locales';
+import LanguageSelect from '../components/LanguageSelect';
+import SvgAnimatedLinearGradient from 'react-native-svg-animated-linear-gradient'
+import { Svg } from 'expo'
+import I18n from 'react-native-i18n'
+import withAuth from "../components/hocs/withAuth";
+
+require('firebase/firestore')
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  container: {
+    backgroundColor: stylesd.corDeFundo,
+  },
+  logoutButtonStyle: {
+    backgroundColor: 'red',
+    marginBottom: 10,
+    borderRadius: 10,
+    marginTop: 25,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+})
+
+class ConfigurationScreen extends Component {
+  state = {
+    loading: true,
+    user: null
+  }
+
+  async updateInfo() {
+    const { user } = this.state
+    const db = firebase.firestore()
+    const dbUsers = db.collection('users')
+    firebase.auth().onAuthStateChanged((currentUser) => {
+      dbUsers.where("authId", "==", currentUser.uid).get()
+      .then( userLogged => {
+        let batch = db.batch();
+        batch.update(userLogged.docs[0].ref, user);
+        batch.commit().catch(err => console.error(err));
+        this.props.navigation.navigate('App')
+        this.forceUpdate()
+      })
+    });
+  }
+
+  logout = () => {
+
+  }
+
+  componentDidMount() {
+    const db = firebase.firestore()
+    const dbUsers = db.collection('users')
+    firebase.auth().onAuthStateChanged((currentUser) => {
+      dbUsers.where("authId", "==", currentUser.uid).get()
+      .then( user => {
+        //   console.log(user.docs[0].data())
+        this.setState({user: user.docs[0].data()}, () => this.setState({loading: false}));
+      })
+    });
+  }
+
+
+  renderLoader = () => {
+    return null;
+  }
+
+  render() {
+    const { loading, user } = this.state
+    if (loading) return (this.renderLoader())
+    return (
+      <Container style={styles.container}>
+      </Container>
+    )
+  }
+}
+
+export default withAuth(ConfigurationScreen)
